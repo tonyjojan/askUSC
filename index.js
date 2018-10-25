f67255177bd6e061d4e1bcbfab53b7ef6dad2884
jojan <tony.jojan@gmail.com>
2018-10-24 17:01:55 -0700

    authroutes file title fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-require('./routes/authRoutes')(app);
+require('./routes/authroutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
